7e15db73f31c8c01ae89935cf52817234e550b33
burak <buraksoftware1@gmail.com>
2023-01-22 14:16:28 +0300

    first commit by burak baba

---
+++ app/src/main/java/com/example/fashionui/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.fashionui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
